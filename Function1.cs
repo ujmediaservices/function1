c11765e5584ddb51d534fd34fef110a0b7555fce
Jay Allen <44067410+ujmediaservices@users.noreply.github.com>
2023-09-19 10:55:11 +0900

    Added feature flag support

--- Function1.cs
+++ Function1.cs
@@ -7,11 +7,23 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Linq;
+using Microsoft.FeatureManagement;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 namespace function1
 {
     public static class Function1
     {
+        private readonly IFeatureManagerSnapshot _featureManagerSnapshot;
+        private readonly IConfigurationRefresher _configurationRefresher;
+        
+        public Function1(IFeatureManagerSnapshot featureManagerSnapshot, IConfigurationRefresherProvider refresherProvider)
+        {
+            _featureManagerSnapshot = featureManagerSnapshot;
+            _configurationRefresher = refresherProvider.Refreshers.First();
+        }
+        
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -19,15 +31,9 @@ namespace function1
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string message = await _featureManagerSnapshot.IsEnabledAsync("feature1")
+                    ? "The Feature Flag 'feature1' is turned ON"
+                    : "The Feature Flag 'feature1' is turned OFF";
 
             return new OkObjectResult(responseMessage);
         }
